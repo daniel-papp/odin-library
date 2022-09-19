32c3e4f2368a31bb0be66a20933c8a56f18425c8
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-09-19 15:45:12 +0200

    Creating function for asking book info and adding it to library

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Initializing the library array for storing books
+
+let myLibrary = [];
+
+// The Book constructor and info method
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -5,16 +11,30 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-// It is best to define functions (methods) on the prototype of that object. Doing so means that a single instance
-// of the function will be shared between all of the Book object. Defining the function in the constructor would
-// duplicate the function each time an object is created.
-
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}.`
 }
 
-const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 295, false);
-console.log(theHobbit.info());
+// Function for asking user info, creating a new book and adding it to the library
+
+function addBookToLibrary() {
+    const newBookTitle = prompt('What is the title?');
+    const newBookAuthor = prompt('Who wrote it?');
+    const newBookPages = prompt('How many pages long is the book?');
+    const newBookRead = prompt('Have you read it?');
+
+    const newBook = new Book(newBookTitle, newBookAuthor, newBookPages, newBookRead);
+    newBook.info();
+    
+    myLibrary.push(newBook);
+    
+    return newBook.info();
+
+}
+
+
+
+// Test book
 
 const neuromancer = new Book('Neuromancer', "William Gibson", 292, true); 
 console.log(neuromancer.info());
